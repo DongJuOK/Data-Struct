d81a50f25eb44ac9b87492c357d04e20753b540f
DongJuOK <kcary0907@gmail.com>
2025-10-29 19:46:40 +0900

    (2025.10.29) 인접 행렬

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -8,31 +8,69 @@ class HashTable
 private:
     struct Node
     {
-        int key;
-        int value;
+        KEY key;
+        VALUE value;
         
         Node * next;
     };
 
     struct Bucket
     {
-        int size;
-        int capacity;
-
-        Bucket * bucket = new Bucket[8];
+        int count;
         Bucket * head;
     };
+
+    int size;
+    int capacity;
+
+    Bucket * bucket;
 
 public:
     HashTable()
     {
-        
+        size = 0;
+        capacity = 8;
+
+        bucket = new Bucket[capacity];
+
+        for (int i = 0; i < capacity; i++)
+        {
+            bucket[i].head = nullptr;
+            bucket[i].count = 0;
+        }
     }
+
+    template <typename KEY>
+    unsigned int hash_function(KEY key)
+    {
+        return (unsigned int)key % capacity;
+    }
+
+    template <>
+    unsigned int hash_function(const char * key)
+    {
+        int sum = 0;
+
+        for (int i = 0; *key != '\0'; i++)
+        {
+            sum += key[i];
+
+            key = key + 1;
+        }
+
+        return (unsigned int)key % capacity;
+    }
+
+
 };
 
 int main()
 {
-    HashTable<int, int> hashtable;
+    HashTable<const char *, int> hashtable;
+
+    cout << hashtable.hash_function("Korea") << endl;
+    cout << hashtable.hash_function("Brazil") << endl;
+    cout << hashtable.hash_function("China") << endl; 
 
     return 0;
 }
